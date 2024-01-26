badb6efd3bef7593690e5d9711f7b4862315dabb
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-26 13:48:09 -0600

    33) Conceptos Finales: Interfaz gráfica - Interfaz básica con un título, una imágen y un botón sin funcionalidad

---
+++ 6_Conceptos_Finales_Python/2_Interfáz_Gráfica_Python/tk_1.py
@@ -0,0 +1,30 @@
+import tkinter as tk 
+from PIL import Image, ImageTk
+
+
+ventana = tk.Tk()
+ventana.title("Hola desde Python")
+ventana.geometry("600x400")
+
+etiqueta = tk.Label(ventana, text='Título de la ventana', fg="#ffffff", bg="blue") # Insertándo un título en la ventana
+# etiqueta.pack(fill=tk.X, expand=True) # Ubicar el texto en el eje Y
+# etiqueta.pack(side=tk.LEFT) # Ubicar el texto en el eje X 
+# etiqueta.pack(fill=tk.BOTH, expand=True) # Para que el cuadro de texto ocupe todo el espacio
+etiqueta.pack() # Cargando el título
+
+boton = tk.Button(ventana, text="Presiona aquí") # Agregando un botón a la ventana
+boton.pack() # Cargando el botón
+
+# Cargar la imagen con Pillow
+imagen_pil = Image.open("logo.jpg")
+
+# Convertir la imagen a un formato compatible con Tkinter
+imagen_tk = ImageTk.PhotoImage(imagen_pil)
+
+# Agregar la imagen a la etiqueta y mostrarla
+label_img = tk.Label(ventana, image=imagen_tk)
+label_img.pack()
+
+
+ventana.mainloop()
+
