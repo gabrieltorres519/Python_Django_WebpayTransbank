23e430537917636e798e0799bc67e569886493a7
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-28 14:04:15 -0600

    49) Django: Implementación del DOM - Creando la primer vista funcional utilizando el sistema de urls y el html de Django

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse, Http404
 # Create your views here.
 
 def home_inicio(request):
-    return HttpResponse("Hola mundo")
+    # return HttpResponse("Hola mundo")
+    return  render(request, 'home/home.html', {}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
+
+# En templates/home habrá un archivo por cada función definida aquí,
+# pues esa función es la encargada de renderizar la vista
