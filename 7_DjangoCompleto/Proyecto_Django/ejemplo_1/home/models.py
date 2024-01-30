4ce402de79b1c4b7ba9aa522f3944241ed62166b
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-30 14:09:15 -0600

    76) D jango: Bases de datos - Haciendo python manage.py makemigrations de varias tablas (clases) a la vez

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
@@ -7,7 +7,7 @@ class Estado(models.Model):
     #id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
     slug = AutoSlugField(populate_from='nombre')
-    numero = models.PositiveIntegerField(default=0) # Cuando se agrega un nuevo campo después de crear la tabla se necesita darle un valor por defecto ya que no estaba contamplado
+    # numero = models.PositiveIntegerField(default=0) # Cuando se agrega un nuevo campo después de crear la tabla se necesita darle un valor por defecto ya que no estaba contamplado
 
 
     def __str__(self):
@@ -18,3 +18,56 @@ class Estado(models.Model):
         db_table = 'estado'
         verbose_name = 'Estado'
         verbose_name_plural = 'Estados' 
+
+
+class Genero(models.Model):
+    nombre = models.CharField(max_length=100, null=True)
+     
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        db_table = 'genero'
+        verbose_name = 'Género'
+        verbose_name_plural = 'Géneros'
+
+
+class Pais(models.Model):
+    nombre = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+    
+    class Meta:
+        db_table = 'pais'
+        verbose_name = 'País'
+        verbose_name_plural = 'Países'
+
+
+class Perfiles(models.Model):
+    nombre = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        db_table = 'perfil'
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfiles'
+
+
+class Metadata(models.Model):
+    description = models.CharField(max_length=255)
+    keyword = models.TextField()
+    correo = models.CharField(max_length=255)
+    telefono = models.CharField(max_length=255)
+    titulo = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.correo
+
+    class Meta:
+        db_table = 'metadata'
+        verbose_name = 'Metadata'
+        verbose_name_plural = 'Metadata'
+    
