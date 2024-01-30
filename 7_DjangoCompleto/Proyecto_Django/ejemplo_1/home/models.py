56a58a632b5f9cb4ed7e832457f37918aa1e11de
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-30 13:23:33 -0600

    72) D jango: Bases de datos - Ejecutando la primer migración con python manage.py migrate y creando las primeras tablas necesarias por defecro en la base de datos a la que se conectó la aplicación, además de la creación del primer modelo, que al migrar nuevamente, se convertirá en una tabla en la base

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Estado(models.Model):
+    #id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+
+    class Meta:
+        # se trata del nombre que tendrá la tabla en la base
+        db_table = 'estado'
+        verbose_name = 'Estado'
+        verbose_name_plural = 'Estados' 
