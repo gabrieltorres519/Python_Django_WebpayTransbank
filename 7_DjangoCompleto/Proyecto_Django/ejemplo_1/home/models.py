f059ee4cb9f4cd3011a84b8a8d8d81128a20e128
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-30 13:43:59 -0600

    73) D jango: Bases de datos - Creando el primer modelo con su respectiva clase y realizando el aviso de migración correspondiente con python manage.py makemigrations

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from autoslug import AutoSlugField
 
 # Create your models here.
 class Estado(models.Model):
     #id = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=100)
+    slug = AutoSlugField(populate_from='nombre')
+
+    def __str__(self):
+        return self.nombre
 
     class Meta:
         # se trata del nombre que tendrá la tabla en la base
