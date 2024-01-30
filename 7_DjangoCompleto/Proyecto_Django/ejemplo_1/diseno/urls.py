e83e453574f062f8e11dbefa71783352fbbf1758
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-30 11:57:07 -0600

    70) D jango: Implemantación de Ajax, plugins js y ventanas modales - Realizando corrección para que cargue fancybox (scripts javascript usan src no href) y modificando data-toggle por data-bs-togel (mostrar model con contenido dinámico usando ajax)

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/diseno/urls.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/diseno/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('', diseno_inicio, name='diseno_inicio'),
     path('/ajax/<int:id>', diseno_ajax, name='diseno_ajax'),
+    path('/modal/<int:id>', diseno_modal, name='diseno_modal')
 ]
 
--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/diseno/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/diseno/views.py
@@ -11,5 +11,10 @@ def diseno_ajax(request, id):
     return  render(request, 'diseno/ajax.html', {'id': id}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
 
 
+def diseno_modal(request, id):
+    # return HttpResponse("Hola mundo")
+    return  render(request, 'diseno/modal.html', {'id': id}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
+
+
 # En templates/home habrá un archivo por cada función definida aquí,
 # pues esa función es la encargada de renderizar la vista
