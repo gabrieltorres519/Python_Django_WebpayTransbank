4ee0bac056d1e3588dd4abe5c1d7c686ea8a1e10
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-01-30 20:20:19 -0600

    84) D jango: Backend - Agregando clases en admin.py de la aplicación home, para poder ver más contenido de más tablas de la base de datos en el sitio administrativo de Django, además de agregar la carpeta llamada utilidades para dar formato a los datos de respuesta (contiene funciones creadas por el profesor del curso para ayudar con el formateo de diferentes datos)

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/admin.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/admin.py
@@ -1,4 +1,37 @@
 from django.contrib import admin
+from home.models import *
+from utilidades import formularios
+
+# Agregamos la configuración para poder tener en la vista la tabla Estado
+class EstadoAdmin(admin.ModelAdmin):
+    list_display = ('id','nombre')
+    search_fields = ('id','nombre')
+
+
+class GeneroAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('id', 'nombre')
+
+
+class PaisAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('id', 'nombre')
+
+
+class PerfilesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre')
+    search_fields = ('id', 'nombre')
+
+
+class UsersMetadataAdmin(admin.ModelAdmin):
+    list_display = ('id', 'telefono', 'genero', formularios.set_user, 'fecha_nacimiento')
+
+
+admin.site.register(Estado, EstadoAdmin)
+admin.site.register(Genero, GeneroAdmin)
+admin.site.register(Pais, PaisAdmin)
+admin.site.register(Perfiles, PerfilesAdmin)
+admin.site.register(UsersMetadata, UsersMetadataAdmin)
 
 admin.site.site_header = 'Backend Ejemplo 1'
 admin.site.index_title = 'Backend Ejemplo 1'
