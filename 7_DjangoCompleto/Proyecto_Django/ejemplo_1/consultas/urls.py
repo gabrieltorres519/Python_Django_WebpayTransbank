316181cc7bd00e8662730bf40a8fd84302b132ec
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-04 14:01:03 -0600

    109) Django: ORM Django - Agregando la vista productos por categoría

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/urls.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import *
  
 urlpatterns = [
-    path('', consultas_inicio, name='consultas_inicio')
+    path('', consultas_inicio, name='consultas_inicio'),
+    path('/productos-por-categoria/<str:slug>', consultas_productos_por_categoria, name='consultas_productos_por_categoria')
 ]
 
--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/views.py
@@ -4,9 +4,12 @@ from home.models import *
 # Create your views here.
 
 def consultas_inicio(request):
-    categorias = Categoria.objects.order_by('?').all()
+    categorias = Categoria.objects.order_by('-id').all()
     return  render(request, 'consultas/home.html', {'categorias':categorias}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
 
+def consultas_productos_por_categoria(request, slug):
+    categorias = Categoria.objects.order_by('-id').all()
+    return  render(request, 'consultas/productos_por_categoria.html', {'categorias':categorias}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
 
 # En templates/home habrá un archivo por cada función definida aquí,
 # pues esa función es la encargada de renderizar la vista
