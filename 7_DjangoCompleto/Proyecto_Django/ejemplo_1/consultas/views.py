929f6ddaef8183efd436bb119a82e8acdc040880
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-04 18:47:13 -0600

    110) Django: ORM Django - Enviando los datos a la url para la visualización de los productos por categoría

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/consultas/views.py
@@ -8,8 +8,15 @@ def consultas_inicio(request):
     return  render(request, 'consultas/home.html', {'categorias':categorias}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
 
 def consultas_productos_por_categoria(request, slug):
-    categorias = Categoria.objects.order_by('-id').all()
-    return  render(request, 'consultas/productos_por_categoria.html', {'categorias':categorias}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
+    try:
+        cat = Categoria.objects.filter(slug = slug).get() # Consulta para un solo registro
+        print(cat.id)
+    except Categoria.DoesNotExist:
+        raise Http404
+     
+    categorias = Categoria.objects.order_by('nombre').all()
+    datos = Producto.objects.filter(categoria_id=cat.id).all()
+    return  render(request, 'consultas/productos_por_categoria.html', {'categorias':categorias, 'cat':cat, 'datos':datos}) # contiene la ruta del template de esa vista y los datos que se quieren renderizar en la vista
 
 # En templates/home habrá un archivo por cada función definida aquí,
 # pues esa función es la encargada de renderizar la vista
