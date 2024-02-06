001804850abd24e9559fe571c018db3b4b835634
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-05 20:40:45 -0600

    120) Django: Template Tags customizados - Obteniendo el título de la tabla Metadata de la base de datos

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/templatetags/utilidades.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/templatetags/utilidades.py
@@ -1,8 +1,27 @@
 from django import template
+from home.models import *
 
 
 register = template.Library()
 
+#######Métodos de bases de datos
+@register.filter(name='getMetadata')
+def getMetadata(n):
+    datos=Metadata.objects.get()
+    lista=[datos.keyword, datos.description, datos.correo, datos.telefono, datos.titulo]
+    if n==1:
+        return datos.keyword
+    if n==2:
+        return datos.description
+    if n==3:
+        return datos.correo
+    if n==4:
+        return datos.telefono
+    if n==5:
+        return datos.titulo
+
+
+#######Métodos de formateo
 @register.filter(name="ejemploFiltro")
 def ejemploFiltro(parametro):
     return f"el valor del parámetro es {parametro}"
