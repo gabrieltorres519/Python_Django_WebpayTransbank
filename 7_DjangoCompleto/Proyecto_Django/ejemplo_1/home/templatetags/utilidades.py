b0914af07919a7feed65c7158ba5a7db5c8e4c55
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-05 20:29:25 -0600

    118) Django: Template Tags customizados - Método para formatear números

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/templatetags/utilidades.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/home/templatetags/utilidades.py
@@ -6,3 +6,11 @@ register = template.Library()
 @register.filter(name="ejemploFiltro")
 def ejemploFiltro(parametro):
     return f"el valor del parámetro es {parametro}"
+
+@register.filter(name='numberFormat')
+def numberFormat(numero):
+    if numero == None:
+        return 0
+    else:
+        return "{:,}".format(numero).replace(",",".")
+    
