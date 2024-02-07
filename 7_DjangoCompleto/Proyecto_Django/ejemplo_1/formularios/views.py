4ac23f634657b730c2cb06c87823a8d9162cb85d
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-06 19:10:53 -0600

    133) Django: Formularios - Implementando mensaje de alerta para después de enviar datos con el formulario simple

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
@@ -12,5 +12,7 @@ def formularios_simple(request):
         #return HttpResponse(f"El valor de nombre es {request.POST['nombre']}")
         descripcion = f"El valor de nombre es {request.POST['nombre']}, correo={request.POST['correo']},  mensaje={request.POST['mensaje']}"
         Tracking.objects.create(descripcion = descripcion) # Haciendo un insert en el campo descripción de la tabla Tracking
+        messages.add_message(request,messages.SUCCESS,f"Gracias por escribirnos, nos pondremos en contacto a la brevedad, sonría que es gratis!!")
+        return HttpResponseRedirect(f"/formularios/simple") # Para evitar que la petición se mantenga guardada en la vista
     return render(request, 'formularios/simple.html',{})
 
