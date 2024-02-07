13effbd75e7d52a2bce09960c61f8c014dd479a9
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-06 18:56:01 -0600

    132) Django: Formularios - Creación de registro en la base de datos desde el formulario

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib import messages 
+from home.models import *
 
 # Create your views here.
 def formularios_inicio(request):
@@ -8,6 +9,8 @@ def formularios_inicio(request):
 
 def formularios_simple(request):
     if request.method == 'POST':
-        return HttpResponse(f"El valor de nombre es {request.POST['nombre']}")
+        #return HttpResponse(f"El valor de nombre es {request.POST['nombre']}")
+        descripcion = f"El valor de nombre es {request.POST['nombre']}, correo={request.POST['correo']},  mensaje={request.POST['mensaje']}"
+        Tracking.objects.create(descripcion = descripcion) # Haciendo un insert en el campo descripción de la tabla Tracking
     return render(request, 'formularios/simple.html',{})
 
