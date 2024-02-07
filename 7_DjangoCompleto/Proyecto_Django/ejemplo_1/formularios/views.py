1c54dc18b50ec6912bb6e81352e2d814e36bb79a
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-06 18:38:44 -0600

    131) Django: Formularios - Enviando respuesta Http después de enviar datos desde el formulario

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/formularios/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.contrib import messages 
 
 # Create your views here.
 def formularios_inicio(request):
     return render(request, 'formularios/home.html',{})
 
 def formularios_simple(request):
+    if request.method == 'POST':
+        return HttpResponse(f"El valor de nombre es {request.POST['nombre']}")
     return render(request, 'formularios/simple.html',{})
 
