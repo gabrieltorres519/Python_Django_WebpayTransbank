f87891d3eca7f71bc04735a1961195cb5be9bc63
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-11 22:27:05 -0600

    153) Django: APIs DjangoREST - Enviando datos de respuesta en lugar de Json

--- 7_DjangoCompleto/Proyecto_Django/ejemplo_1/api/views.py
+++ 7_DjangoCompleto/Proyecto_Django/ejemplo_1/api/views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.http import Http404, HttpResponse
 
 class Class_Test(APIView):
     
     # Petición vía GET
     def get(self, request):
-        return Response({"mensaje": "Hola desde mi API"})
+        #return Response({"mensaje": "Hola desde mi API"}) #envío de JSON
+        return HttpResponse("Datos de respuesta")
