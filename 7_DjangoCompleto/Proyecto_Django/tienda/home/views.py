5687511cd92b3f9ba81ba1df370813feb2bdbd8c
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-14 18:43:04 -0600

    180) Django: Tienda Virtual Webpay de Transbank - Obteniendo los productos de la base de datos y mostrándolos con un ciclo for

--- 7_DjangoCompleto/Proyecto_Django/tienda/home/views.py
+++ 7_DjangoCompleto/Proyecto_Django/tienda/home/views.py
@@ -5,6 +5,6 @@ from home.models import *
 
 def home_inicio(request):
 	#select * from productos where estado_id=1 order by id desc limit 8
-	datos="Producto.objects.filter(estado_id=1).order_by('-id').all()[:8]"
+	datos=Producto.objects.filter(estado_id=1).order_by('-id').all()[:8]
 	return render(request, 'home/home.html', {'datos':datos})
 
