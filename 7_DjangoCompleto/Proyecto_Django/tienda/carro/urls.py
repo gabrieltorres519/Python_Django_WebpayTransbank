14c0f1a95abec3e02d2654405762b878fa547c3e
gabrieltorres519 <gabrieltorres9909@gmail.com>
2024-02-16 10:26:04 -0600

    186) Django: Tienda Virtual Webpay de Tr ansbank - Configuraciones iniciales para el carrrito de compra

---
+++ 7_DjangoCompleto/Proyecto_Django/tienda/carro/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+	path('', carro_inicio, name="carro_inicio"),
+	path('crear', carro_crear, name="carro_crear"),
+	# path('vaciar', carro_vaciar, name="carro_vaciar"),
+	# path('quitar-de-carro/<int:id>', carro_quitar, name="carro_quitar"),
+	# path('modificar-cantidad-carro/<int:id>/<int:cantidad>', carro_modificar_cantidad, name="carro_modificar_cantidad"),
+	# path('pagar', carro_pagar, name="carro_pagar"),
+	# path('webpay', carro_webpay, name="carro_webpay"),
+	# path('webpay-respuesta', carro_webpay_respuesta, name="carro_webpay_respuesta"),
+]
--- 7_DjangoCompleto/Proyecto_Django/tienda/carro/views.py
+++ 7_DjangoCompleto/Proyecto_Django/tienda/carro/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.contrib import messages
+from home.models import *
+from acceso.decorators import logueado
+from utilidades import utilidades, webpay
+import json
+from django.conf import settings
 
 # Create your views here.
+@logueado()
+def carro_inicio(request):
+    cuantos=Carrito.objects.filter(users_metadata_id=request.session['users_metadata_id']).count()
+    datos=Carrito.objects.filter(users_metadata_id=request.session['users_metadata_id']).order_by('-id').all()
+    suma=0
+    for dato in datos:
+        valor=dato.cantidad*dato.producto.precio
+        suma=suma+valor
+    return render(request, 'carro/home.html', {'datos': datos, 'suma': suma, 'cuantos': cuantos})
+
+
+@logueado()
+def carro_crear(request):
+    if request.method =='POST':
+        try:
+            datos=Producto.objects.filter(pk=request.POST['id']).get()
+        except Producto.DoesNotExist:
+            raise Http404
+        Carrito.objects.create(cantidad=request.POST['cantidad'], producto_id=request.POST['id'], users_metadata_id=request.session['users_metadata_id'])
+        return HttpResponseRedirect("/carro")
+    else:
+        raise Http404
